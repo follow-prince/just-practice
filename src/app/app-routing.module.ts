295386cbfa178b074e5b7e13a6c8774a0bcb8037
elavarasan <elavarasa.003@gmail.com>
2023-01-20 16:15:58 +0000

    just  fun

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { ValliComponent } from './valli/valli.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VennilaComponent } from './vennila/vennila.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"/", pathMatch:"full"},
+  {path:"valli", component:ValliComponent},
+  {path:"vennila", component:VennilaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const allRoutes =[
+                            ValliComponent,
+                            VennilaComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, allRoutes } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValliComponent } from './valli/valli.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValliComponent
+    allRoutes
   ],
   imports: [
     BrowserModule,
